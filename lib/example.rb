81fd7a7e7417f81b86d42d4e2333053c0e075780
Romena Murgor <romenamurgor@gmail.com>
2020-03-06 17:49:29 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,3 +1,13 @@
-def 
+def say_hello_ten_times
 phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase 
+puts phrase
+puts phrase
+puts phrase
 end
